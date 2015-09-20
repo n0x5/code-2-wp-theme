768561e130ac8648b1049820d8cab2915adb417d
n0x5 <coax@audiolabs.org>
2015-09-20 02:03:11 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -43,53 +43,6 @@ add_action( 'wp_enqueue_scripts', 'code2_scripts' );
 
 add_theme_support( 'post-thumbnails' );
 
-/*
-function movies_post_type() {
-
-	$labels = array(
-		'name'                => 'Movies',
-		'singular_name'       => 'Movie',
-		'menu_name'           => 'Movies',
-		'parent_item_colon'   => 'Parent Item:',
-		'all_items'           => 'All Items',
-		'view_item'           => 'View Item',
-		'add_new_item'        => 'Add New Item',
-		'add_new'             => 'Add New',
-		'edit_item'           => 'Edit Item',
-		'update_item'         => 'Update Item',
-		'search_items'        => 'Search Item',
-		'not_found'           => 'Not found',
-		'not_found_in_trash'  => 'Not found in Trash',
-	);
-	$args = array(
-		'label'               => 'movies_post_type',
-		'description'         => 'Post Type Description',
-		'labels'              => $labels,
-		'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'custom-fields', 'page-attributes', 'post-formats', ),
-		'taxonomies'          => array( 'category', 'post_tag' ),
-		'hierarchical'        => true,
-		'public'              => true,
-		'show_ui'             => true,
-		'show_in_menu'        => true,
-		'show_in_nav_menus'   => true,
-		'show_in_admin_bar'   => true,
-		'menu_position'       => 5,
-		'menu_icon'           => 'dashicons-editor-video',
-		'can_export'          => true,
-		'has_archive'         => true,
-		'exclude_from_search' => false,
-		'publicly_queryable'  => true,
-		'capability_type'     => 'page',
-	);
-	register_post_type( 'movies_post_type', $args );
-
-}
-
-
-
-// Hook into the 'init' action
-add_action( 'init', 'movies_post_type', 0 );
-*/
 $markup = array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption', );
 add_theme_support( 'html5', $markup );	
 
