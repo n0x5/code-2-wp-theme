d3019a56db2d74ce1e75e22fbd5a10cb218854ec
n0x5 <coax@audiolabs.org>
2015-11-21 18:09:18 +0100

    Update content.php

--- content.php
+++ content.php
@@ -1,18 +1,17 @@
-<div class="content">
-		<div class="metainfo">
-		<div class="metatitle1">
-		<?php the_time('F jS, Y') ?><br />
-		<?php the_category(', '); ?><br />
-
-<span class="post-format">
-                                <a class="format-standard" href="?post_format=standard"><?php echo get_post_format_string( 'standard' ); ?></a>
-                        </span>
-
-
-                </div><br />
-		</div>
+	
+  <div class="content">
 		<div class="entry">
+
+
 			<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
-			<div class="post-entry"><?php the_content('more))'); ?></div>
-		</div>
-</div>
+
+                        
+			<div class="post-entry"> <?php  the_content('more))');  ?></div>
+
+
+
+                        <div class="metatitle1"><?php the_time('F jS, Y') ?> <img src="<?php echo get_bloginfo ( 'template_directory' );   ?>/images/sitelogo.png" alt="Site Logo"></div>
+	
+
+         </div>
+</div>	
